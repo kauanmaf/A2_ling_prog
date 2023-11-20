1c53eaacca19314be201f144b666c3ddeafd2472
Kauan Mariani Ferreira <kauan.mariani@gmail.com>
2023-11-20 18:01:29 -0300

    adicionando mais uma lógica nos games

--- levels/overworld.py
+++ levels/overworld.py
@@ -26,11 +26,12 @@ class Icon(pygame.sprite.Sprite):
         self.rect.center = self.pos
 
 class Overworld:
-    def __init__(self, start_level, max_level, surface):
+    def __init__(self, start_level, max_level, surface, create_level):
         # setup
         self.display_surface = surface
         self.max_level = max_level
         self.current_level = start_level
+        self.create_level = create_level
 
         # movement logic
         self.move_direction = pygame.math.Vector2(0,0)
@@ -71,10 +72,12 @@ class Overworld:
                 self.move_direction = self.get_movement_data("next")
                 self.current_level += 1
                 self.moving = True
-            elif keys[pygame.K_LEFT] and self.current_level > 0:
+            elif keys[pygame.K_LEFT] and self.current_level != 0:
                 self.move_direction = self.get_movement_data("previous")
                 self.current_level -= 1
                 self.moving = True
+            elif keys[pygame.K_SPACE]:
+                self.create_level(self.current_level)
 
     def get_movement_data(self, target):
         start = pygame.math.Vector2(self.nodes.sprites()[self.current_level].rect.center)
