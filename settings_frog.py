c6c57108c0cd890bc9537eb4040e5e0c7b0093c0
Guilherme Carvalho <cguilherme182@gmail.com>
2023-12-09 13:01:14 -0300

    add assets credits and rename assets folder

--- settings_frog.py
+++ settings_frog.py
@@ -10,15 +10,15 @@ screen_size = (screen_width, screen_height)
 screen = pygame.display.set_mode(screen_size)
 
 # assets frog game
-fg_background1 = pygame.image.load("./assets/background1.png")
-fg_background2 = pygame.image.load("./assets/background2.png")
-lilypad1 = pygame.image.load("./assets/lilypad1.png")
-lilypad2 = pygame.image.load("./assets/lilypad2.png")
-lilypad3 = pygame.image.load("./assets/lilypad3.png")
-alligator = pygame.image.load("./assets/crocodile.png")
-frog_img = pygame.image.load("./assets/froginho.png")
-death_box = pygame.image.load("./assets/deathbox1.png")
-dead_frog = pygame.image.load("./assets/dead_frog'.png")
+fg_background1 = pygame.image.load("./assets_frog/background1.png")
+fg_background2 = pygame.image.load("./assets_frog/background2.png")
+lilypad1 = pygame.image.load("./assets_frog/lilypad1.png")
+lilypad2 = pygame.image.load("./assets_frog/lilypad2.png")
+lilypad3 = pygame.image.load("./assets_frog/lilypad3.png")
+alligator = pygame.image.load("./assets_frog/crocodile.png")
+frog_img = pygame.image.load("./assets_frog/froginho.png")
+death_box = pygame.image.load("./assets_frog/deathbox1.png")
+dead_frog = pygame.image.load("./assets_frog/dead_frog'.png")
 
 # first background image
 fg_current_image = fg_background1
@@ -27,12 +27,12 @@ obstacle_images = [lilypad1, lilypad2, lilypad3, alligator]
 
 # sound effects frog game
 # soundtrack working
-pygame.mixer.music.load("./assets/a_frog_adventure.mp3")
+pygame.mixer.music.load("./assets_frog/a_frog_adventure.mp3")
 pygame.mixer.music.set_volume(0.5)
 pygame.mixer.music.play(-1)
 
 # collision sound
-fg_collision_sound = pygame.mixer.Sound("./assets/wet_frog.mp3")
+fg_collision_sound = pygame.mixer.Sound("./assets_frog/wet_frog.mp3")
 fg_collision_sound.set_volume(0.5)
 
 # cores 
