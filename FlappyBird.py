ffa854a5b028e352bc64cab23dce9695830b24f7
Kauan Mariani Ferreira <kauan.mariani@gmail.com>
2023-12-10 01:16:14 -0300

    adicionando nomenclaturas melhores

--- FlappyBird.py
+++ FlappyBird.py
@@ -1,6 +1,7 @@
 import pygame
 import random
-from settings import *
+import sys
+from settings_flappy import *
 from abstract import *
 from classes_base_flappy import *
 
@@ -288,3 +289,37 @@ class FlappyBird(Minigame_abs):
         else:
             self.draw()
             self.update()
+
+if __name__ == "__main__":
+    # Configurando o jogo                                     
+    pygame.init()
+    screen = pygame.display.set_mode((screen_width, screen_height))
+    clock = pygame.time.Clock()
+    game = FlappyBird(screen)
+
+    # Adicionando nome
+    pygame.display.set_caption("Mini Arcade")
+
+    # making the loop while to start
+    while True:
+        # Configurando a taxa de atualização do jogo
+        clock.tick(60)
+
+        # Configurando o fundo da tela
+        screen.fill((0, 0, 0))
+
+        # Criando uma instância do jogo
+        game.run()
+
+        # Loop for para casa a pessoa queira sair do jogo
+        for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_ESCAPE:
+                        pygame.quit()
+                        sys.exit()
+
+        # Fazendo um update pra manter no caso de atualizações
+        pygame.display.update()
